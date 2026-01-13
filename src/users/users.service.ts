090d1a5aadc92675bd3bebc802f87465d75ac3bf
valentin-vinel <valentinvnl84@gmail.com>
2026-01-13 13:34:04 +0100

    feat: add user type and User array in service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'types/User.type';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: User[] = [
+        {
+            id: "1",
+            name: "John Doe",
+            email: "john.doe@example.com",
+            role: "admin",
+        },
+        {
+            id: "2",
+            name: "Jim Doe",
+            email: "jim.doe@example.com",
+            role: "admin",
+        },
+        {
+            id: "3",
+            name: "Jill Doe",
+            email: "jill.doe@example.com",
+            role: "admin",
+        },
+        {
+            id: "4",
+            name: "Jack Doe",
+            email: "jack.doe@example.com",
+            role: "admin",
+        },
+    ]
+}
