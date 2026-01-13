8ec8d367a9c6a73ac88e8f11bb1569936e711600
valentin-vinel <valentinvnl84@gmail.com>
2026-01-13 13:56:53 +0100

    feat: add @Get findAll to users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from 'types/User.type';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService : UsersService) {};
+
+    @Get()
+    @HttpCode(200)
+    findAll(): User[] {
+        return this.usersService.findAll();
+    }
+}
