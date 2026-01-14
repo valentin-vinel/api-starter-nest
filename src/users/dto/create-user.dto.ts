3c5ed5422b7d2b6758ef9004860b0889b05c28b5
valentin-vinel <valentinvnl84@gmail.com>
2026-01-14 10:39:59 +0100

    feat: add CreateUserDto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsString({ message: "Le nom doit être une chaîne de caractères"})
+    @IsNotEmpty({ message: "Le nom est requis" })
+    @MinLength(3, { message: "Le nom doit comporter au minimum 3 caractères"})
+    @MaxLength(20, { message: "Le nom doit comporter au maximum 20 caractères"})
+    name: string;
+    @IsEmail({}, { message: "L'email doit être une adresse email valide" })
+    @IsNotEmpty({ message: "L'email est requis" })
+    email: string;
+    @IsOptional()
+    @IsEnum(['admin', 'user'], { message: "Le rôle doit être admin ou user"})
+    role?: 'admin' | 'user';
+}
