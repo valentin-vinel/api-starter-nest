9356003478bc6275ea9efc26058a255ecc81d80a
valentin-vinel <valentinvnl84@gmail.com>
2026-01-15 16:07:28 +0100

    fix: npm vulnerability

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:4200'],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Range', 'X-Total-Count'],
+    maxAge: 600,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   // app.useGlobalGuards(new AuthGuard());
   await app.listen(process.env.PORT ?? 3000);
 }
